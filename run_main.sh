9da31a5e6ddd18098485235816c22f968e32020c
Aliya Ablitip <101862930+AliyaAbl@users.noreply.github.com>
2023-10-04 18:23:32 -0400

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=nwb_file_creation
-#SBATCH --array=0-55
+#SBATCH --array=0-201
 #SBATCH --time=01:00:00
 #SBATCH --output=spikes.txt
 #SBATCH --ntasks=1
-#SBATCH --mem=8000
+#SBATCH --mem=3000
 #SBATCH --exclude=node[066,067,115]
 #SBATCH -c4
 #SBATCH -p dicarlo
